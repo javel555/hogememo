298b2f9951865b76558924e91b1a0220e57c8c1c
javel555 <javel555@gmail.com>
2017-08-25 01:22:51 +0900

    done twitter callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
-  devise :omniauthable
+  devise :omniauthable, omniauth_providers: [:twitter]
+
+  def self.from_omniauth(auth)
+    find_or_create_by!(provider: auth["provider"], uid: auth["uid"]) do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new( session["devise.user_attributes"] ) do |user|
+        user.attributes = params
+      end
+    else
+      super
+    end
+  end
 end
