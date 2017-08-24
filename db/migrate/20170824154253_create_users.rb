e77a6ae49612f3d5b40f10c66305a5d6456cf5d5
javel555 <javel555@gmail.com>
2017-08-25 00:44:02 +0900

    add user model

---
+++ db/migrate/20170824154253_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+
+      t.timestamps
+    end
+
+    add_index :users, [:provider, :uid], unique: true
+
+  end
+end
