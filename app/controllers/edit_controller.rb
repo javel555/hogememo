1fb5bdc700cf2ea273bbccda629453ade6dbfa8e
javel555 <javel555@gmail.com>
2017-09-24 11:57:20 +0900

    no message

--- app/controllers/edit_controller.rb
+++ app/controllers/edit_controller.rb
@@ -2,6 +2,14 @@ class EditController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @keyword = Searchcondition.where(uid_id: current_user.uid).first.keyword
     @tweets = Tweet.where(uid_id: current_user.uid)
+  end
+
+  def add_tag
+    tweet = Tweet.find(params[:id])
+    tweet.tag_list.add(params[:append_tag])
+    tweet.save
+    redirect_to edit_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+
   get 'edit', to: 'edit#show'
+  post 'edit/tag/:id', to: 'edit#add_tag'
+
   post 'search', to: 'search#update'
+
+
 
 
   devise_for :users, :controllers => { :omniauth_callbacks => "user/omniauth_callbacks"}
