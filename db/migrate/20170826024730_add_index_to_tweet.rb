bbad35b6e03ef9474bb76c1c3edea3733d431259
javel555 <javel555@gmail.com>
2017-10-07 14:35:55 +0900

    上手くいかなかったので修正

--- db/migrate/20170826024730_add_index_to_tweet.rb
+++ db/migrate/20170826024730_add_index_to_tweet.rb
@@ -1,6 +1,6 @@
 class AddIndexToTweet < ActiveRecord::Migration[5.1]
   def change
-    change_column :tweets, :uuid, limit: 128
+    change_column :tweets, :uuid, :string, limit: 128
     add_index :tweets, :uuid, unique: true
   end
 end
