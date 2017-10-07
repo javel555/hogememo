508b8e469bee958cd48d3906727fd1f4c9fae426
javel555 <javel555@gmail.com>
2017-10-07 14:02:19 +0900

    Mysqlがインデックスに使えるカラムの長さを制限してくるので対応

--- db/migrate/20170824154253_create_users.rb
+++ db/migrate/20170824154253_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
+      t.string :provider, limit: 128
+      t.string :uid, limit: 128
 
       t.timestamps
     end
--- db/migrate/20170825132654_create_create_searchconditions.rb
+++ db/migrate/20170825132654_create_create_searchconditions.rb
@@ -1,8 +1,8 @@
 class CreateCreateSearchconditions < ActiveRecord::Migration[5.1]
   def change
     create_table :searchconditions do |t|
-      t.string :uid_id
-      t.string :keyword
+      t.string :uid_id, limit: 128
+      t.string :keyword, limit: 128
 
       t.timestamps
     end
